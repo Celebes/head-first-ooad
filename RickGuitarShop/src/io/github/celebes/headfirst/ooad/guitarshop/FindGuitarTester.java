fddb8e4fb9ae41c93edbf5046a89778e4fb971bc
Celebeze <kris1337@gmail.com>
2014-09-07 19:55:55 +0200

    Poprawiono wypisywanie wynikow wyszukiwania

--- RickGuitarShop/src/io/github/celebes/headfirst/ooad/guitarshop/FindGuitarTester.java
+++ RickGuitarShop/src/io/github/celebes/headfirst/ooad/guitarshop/FindGuitarTester.java
@@ -11,20 +11,23 @@ public class FindGuitarTester {
 		Inventory inventory = new Inventory();
 		initializeInventory(inventory);
 
-		GuitarSpec whatErinLikes = new GuitarSpec(Builder.FENDER, "Stratocastor", Type.ELECTRIC, 6, Wood.ALDER, Wood.ALDER);
+		GuitarSpec whatErinLikes = new GuitarSpec(Builder.FENDER,
+				"Stratocastor", Type.ELECTRIC, 6, Wood.ALDER, Wood.ALDER);
 
 		List<Guitar> foundGuitars = inventory.search(whatErinLikes);
 
 		if (!foundGuitars.isEmpty()) {
+			System.out.println("Erin, you might like these guitars:");
 			for (Guitar guitar : foundGuitars) {
 				GuitarSpec spec = guitar.getSpec();
 
-				System.out.println("Erin, you might like this "
-						+ spec.getBuilder() + " " + spec.getModel() + " "
-						+ spec.getType() + " guitar:\n   " + spec.getBackWood()
-						+ " back and sides,\n   " + spec.getTopWood()
-						+ " top.\nYou can have it for only $"
-						+ guitar.getPrice() + "!\n ----");
+				System.out.println("  We have a " + spec.getBuilder() + " "
+						+ spec.getModel() + " " + spec.getNumStrings()
+						+ "-string " + spec.getType() + " guitar:\n     "
+						+ spec.getBackWood() + " back and sides,\n     "
+						+ spec.getTopWood()
+						+ " top.\n  You can have it for only $"
+						+ guitar.getPrice() + "!\n  ----");
 			}
 		} else {
 			System.out.println("Sorry, Erin, we have nothing for you.");
@@ -32,8 +35,9 @@ public class FindGuitarTester {
 	}
 
 	private static void initializeInventory(Inventory inventory) {
-		GuitarSpec erinSpec = new GuitarSpec(Builder.FENDER, "Stratocastor", Type.ELECTRIC, 6, Wood.ALDER, Wood.ALDER);
-		
+		GuitarSpec erinSpec = new GuitarSpec(Builder.FENDER, "Stratocastor",
+				Type.ELECTRIC, 6, Wood.ALDER, Wood.ALDER);
+
 		inventory.addGuitar("V95693", 1499.95, erinSpec);
 		inventory.addGuitar("V9512", 1549.95, erinSpec);
 	}
