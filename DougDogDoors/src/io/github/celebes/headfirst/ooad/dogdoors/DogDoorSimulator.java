ad900dc6fea9ead9243aaa2ae77b301488dd452d
Celebeze <kris1337@gmail.com>
2014-09-11 21:02:19 +0200

    Zmieniono testy, zeby uwzglednic przypadek, w ktorym pies nie zdazy wrocic przed automatycznym zamknieciem drzwi

--- DougDogDoors/src/io/github/celebes/headfirst/ooad/dogdoors/DogDoorSimulator.java
+++ DougDogDoors/src/io/github/celebes/headfirst/ooad/dogdoors/DogDoorSimulator.java
@@ -5,13 +5,25 @@ public class DogDoorSimulator {
 		DogDoor door = new DogDoor();
 		Remote remote = new Remote(door);
 		
-		System.out.println("Fido barks to go outside...");
+		System.out.println();
+		
 		remote.pressButton();
 		
 		System.out.println("\nFido has gone outside...");
-
 		System.out.println("\nFido is all done...");
 		
-		System.out.println("\nFido is back inside...");
+		try {
+			Thread.currentThread().sleep(10000);
+		} catch(InterruptedException e) {
+			
+		}
+		
+		System.out.println("...but he's stuck outside!");
+		System.out.println("\nFido starts barking...");
+		System.out.println("...so Gina grabs the remote control.");
+		
+		remote.pressButton();
+		
+		System.out.println("\nFido's back inside...");
 	}
 }
